d25bbcb1b3d308e8fe973e846a23d1a9cc3e068b
jongho <whjk1104@gmail.com>
2024-12-26 16:47:09 +0900

    ✨ Setting comment/model.mjs file
    setting comment/model.mjs file

--- src/feature/comment/model.mjs
+++ src/feature/comment/model.mjs
@@ -1,21 +1,51 @@
-
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-// 여러 개 조회할 때는 pagination 기능을 넣는다.
-export const filmFindMany = async (pageStart, pageEnd) => {
-  return prisma.film.findMany({
+export const commentFindMany = async (pageStart, pageEnd, post_id) =>
+  prisma.film_comment.findMany({
+    where: {
+      post_id,
+    },
+    include: {
+      customer: {
+        select: {
+          first_name: true,
+          last_name: true,
+        },
+      },
+    },
+    orderBy: {
+      created_at: "desc",
+    },
     skip: pageStart,
     take: pageEnd,
   });
-};
 
-export const filmFindOne = async (film_id) => {
-  return prisma.film.findUnique({
-    where: {
-      film_id: film_id,
+export const commentCreate = async ({ post_id, customer_id, content }) =>
+  prisma.film_comment.create({
+    data: {
+      content,
+      post_id,
+      customer_id,
     },
   });
-};
 
-// findFirst
+// 수정한 댓글 데이터베이스에 업데이트하기 
+export const commentUpdate = async (comment_id, content) =>
+  prisma.film_comment.update({  
+    where: {
+      comment_id,
+    },
+    data: {
+      content,
+      updated_at: new Date(),
+    },
+  });
+
+// 댓글 데이터베이스에서 삭제하기 
+export const commentDelete = async (comment_id) =>
+  prisma.film_comment.delete({
+    where: {
+      comment_id,
+    },
+  });
