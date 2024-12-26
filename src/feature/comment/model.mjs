9a2a05f88a93a3cb303e4dbf62764b889cc984e0
jongho <whjk1104@gmail.com>
2024-12-26 16:37:36 +0900

    ✨ Setting model file
    setting model file

--- src/feature/comment/model.mjs
+++ src/feature/comment/model.mjs
@@ -1,31 +1,21 @@
+
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export const commentFindMany = async (pageStart, pageEnd, post_id) =>
-  prisma.film_comment.findMany({
-    where: {
-      post_id,
-    },
-    include: {
-      customer: {
-        select: {
-          first_name: true,
-          last_name: true,
-        },
-      },
-    },
-    orderBy: {
-      created_at: "desc",
-    },
+// 여러 개 조회할 때는 pagination 기능을 넣는다.
+export const filmFindMany = async (pageStart, pageEnd) => {
+  return prisma.film.findMany({
     skip: pageStart,
     take: pageEnd,
   });
+};
 
-export const commentCreate = async ({ post_id, customer_id, content }) =>
-  prisma.film_comment.create({
-    data: {
-      content,
-      post_id,
-      customer_id,
+export const filmFindOne = async (film_id) => {
+  return prisma.film.findUnique({
+    where: {
+      film_id: film_id,
     },
   });
+};
+
+// findFirst
